8cfcdc417110a39ff9e16f74a6367d41d60febef
Naimas <Naimas@DESKTOP-68Q4MHG>
2023-06-21 21:29:07 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введіть число для перевірки:");
+        int number = int.Parse(Console.ReadLine());
+
+        bool isPalindrome = CheckPalindrome(number);
+
+        Console.WriteLine($"Число {number} є паліндромом: {isPalindrome}");
+
+        Console.ReadLine();
+    }
+
+    static bool CheckPalindrome(int number)
+    {
+        string numberString = number.ToString();
+        int left = 0;
+        int right = numberString.Length - 1;
+
+        while (left < right)
+        {
+            if (numberString[left] != numberString[right])
+            {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
